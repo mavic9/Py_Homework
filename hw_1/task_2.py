1f38f8737cb5e4a5e3e7d2d81dd6d434c5405d06
Victor Mamontov <38543150+mavic9@users.noreply.github.com>
2021-07-19 17:22:30 +0300

    update task_2.py

--- hw_1/task_2.py
+++ hw_1/task_2.py
@@ -6,25 +6,4 @@ hh = int(sec//3600)
 mm = int((sec%3600)//60)
 ss = int(sec%60)
 
-if hh > 9:                          #условие для соблюдения формата вывода
-    if mm > 9:
-        if ss > 9:
-            print(f'{hh}:{mm}:{ss}')
-        else:
-            print(f'{hh}:{mm}:0{ss}')
-    else:
-        if ss > 9:
-            print(f'{hh}:0{mm}:{ss}')
-        else:
-            print(f'{hh}:0{mm}:0{ss}')
-else:
-    if mm > 9:
-        if ss > 9:
-            print(f'0{hh}:{mm}:{ss}')
-        else:
-            print(f'0{hh}:{mm}:0{ss}')
-    else:
-        if ss > 9:
-            print(f'0{hh}:0{mm}:{ss}')
-        else:
-            print(f'0{hh}:0{mm}:0{ss}')
+print(f'{hh:02}:{mm:02}:{ss:02}')
