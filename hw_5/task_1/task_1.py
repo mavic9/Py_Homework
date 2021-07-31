79e751200d800e8cfcb69c303dc6cfcae0d772f2
Victor <victor221093@gmail.com>
2021-07-31 21:04:38 +0300

    solve task 1

---
+++ hw_5/task_1/task_1.py
@@ -0,0 +1,25 @@
+# Author: Viktor Mamontov
+# Script for writing of message in txt file
+
+def file_gen(new_file):
+    phrase = input("Enter your phrase: ")
+    phrase_list = [phrase + '\n']
+    my_stop = False
+    new_obj = open(new_file, 'w')
+    if phrase != '':
+        while not my_stop:
+            new_phrase = input('Enter your phrase (or press Enter to stop) : ')
+            if new_phrase != '':
+                phrase_list.append(new_phrase + '\n')
+            else:
+                my_stop = True
+        new_obj.writelines(phrase_list)
+        new_obj.close()
+    else:
+        print('No message in your phrase!')
+    print('New file is written!')
+    return
+
+
+file_name = input('Enter file name in format name.txt: ')
+file_gen(file_name)
