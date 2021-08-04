e6acc72c99536dde3a74f7e90b6cf2677247ab37
Victor <victor221093@gmail.com>
2021-08-04 16:36:54 +0300

    update task 4

--- hw_6/task_3.py
+++ hw_6/task_3.py
@@ -15,10 +15,14 @@ class Position(Worker):
     def get_full_name(self):
         print(f'Full name: {self.name} {self.surname}')
 
+    def get_position(self):
+        print(f'Position: {self.position}')
+
     def get_total_income(self):
         print(f'Total income: {sum(self._income.values())}')
 
 
-position_1 = Position('Ivan', 'Petrov', 'analytic', {'wage': 100000, 'bonus': 25000})
+position_1 = Position('Ivan', 'Petrov', 'Data Analyst', {'wage': 100000, 'bonus': 25000})
 position_1.get_full_name()
+position_1.get_position()
 position_1.get_total_income()
