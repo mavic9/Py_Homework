6d317085cdd9d1fc9aeaf939420f46fc77668fe8
Victor <victor221093@gmail.com>
2021-08-08 23:20:46 +0300

    update hw_6/task_2

--- hw_6/task_2.py
+++ hw_6/task_2.py
@@ -7,12 +7,13 @@ class Road:
         self._length = length  # length of road in m
         self._width = width  # width of road in m
 
-    def make_count(self):
-        density = 2.5  # density of asphalt in tons per 1 cubic m
-        height = 0.05  # height of asphalt in m
-        total_mass = self._length * self._width * density * height
+    def make_count(self, density, height):
+        """:param density in kg
+           :param height in cm
+        """
+        total_mass = self._length * self._width * density * height / 1000
         print(f'A total mass of asphalt: {total_mass} t')
 
 
 new_road = Road(20, 5000)
-new_road.make_count()
+new_road.make_count(25, 5)
