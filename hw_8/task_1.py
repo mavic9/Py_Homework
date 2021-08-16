8bc0f7411fa031703dfcd8dc2fd10f3c8bf738ee
Victor <victor221093@gmail.com>
2021-08-16 10:59:01 +0300

    update hw_8/task_1

--- hw_8/task_1.py
+++ hw_8/task_1.py
@@ -16,21 +16,46 @@ class Data:
     @staticmethod
     def test_int(my_data):
         my_list = my_data.split('-')
-        if 0 < int(my_list[0]) < 32:
-            if 0 < int(my_list[1]) < 13:
-                if 0 < int(my_list[2]) < 2100:
-                    return print(f'The data {my_data} is correct!')
+        a = int(my_list[0])
+        b = int(my_list[1])
+        c = int(my_list[2])
+        if b != 2:
+            if b == 4 or b == 6 or b == 9 or b == 11:
+                if 0 < a < 31:
+                    if 0 < b < 13:
+                        if 0 < c < 2100:
+                            return print(f'The data {my_data} is correct!')
+                        else:
+                            return print(f'Tne year {c} is incorrect!!!')
+                    else:
+                        return print(f'The month {b} is incorrect!!!')
                 else:
-                    return print(f'Tne year {my_list[2]} is incorrect!!!')
+                    return print(f'The day {a} is incorrect!!!')
             else:
-                return print(f'The month {my_list[1]} is incorrect!!!')
+                if 0 < a < 32:
+                    if 0 < b < 13:
+                        if 0 < c < 2100:
+                            return print(f'The data {my_data} is correct!')
+                        else:
+                            return print(f'Tne year {c} is incorrect!!!')
+                    else:
+                        return print(f'The month {b} is incorrect!!!')
+                else:
+                    return print(f'The day {a} is incorrect!!!')
+        elif c % 4 == 0 and a < 30:
+            return print(f'The data {my_data} is correct!')
+        elif a < 29:
+            return print(f'The data {my_data} is correct!')
         else:
-            return print(f'The day {my_list[0]} is incorrect!!!')
+            return print(f'The day {a} is incorrect!!!')
 
 
-data_1 = '10-12-1990'
-data_2 = '31-14-2000'
+data_1 = '29-02-1990'
+data_2 = '31-12-2000'
+data_3 = '29-02-2000'
 print(Data.get_int(data_1))
 Data.test_int(data_1)
 print(Data.get_int(data_2))
 Data.test_int(data_2)
+print(Data.get_int(data_3))
+Data.test_int(data_3)
